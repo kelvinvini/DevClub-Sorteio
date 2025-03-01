85af8de9ed4b243225ed680db2ce6ab0e91a1aea
kelvinvini <kelvinapol@gmail.com>
2025-03-01 16:59:52 -0300

    alteração no alert do js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ function generalnumber() {
     const min = Math.ceil (document.querySelector(".input-main").value)
     const max = Math.floor (document.querySelector(".input-right").value)
 
+    if (max > min) {  
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    
-
-    alert(result)
+     alert (result)
+} else {  
+alert ("O valor minimo tem que ser MENOR que o valor máximo")
 }
 
+}
 
-    
